ace57b1518ab4ee9a4714b7b5bf58927a72e8816
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-04-26 15:46:17 +0900

    add room CRUD(index, show)

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,53 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_pramas)
+    if @room.save
+      redirect_to room_path(@room.id)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_pramas)
+      redirect_to room_path(@room.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to rooms_path
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params[:room].permit(
+      :name,
+      :address,
+      :rent,
+      :nearest_station,
+      :comment
+    )
+  end
+end
--- db/migrate/20210426061124_create_rooms.rb
+++ db/migrate/20210426061124_create_rooms.rb
@@ -1,7 +1,11 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.string "name"
+      t.string "address"
+      t.integer "rent"
+      t.string "nearest_station"
+      t.text "comment"
       t.timestamps
     end
   end
