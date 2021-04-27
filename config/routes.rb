de49850148a8ab99eddf42ab4d9668364708a62f
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-04-27 17:28:01 +0900

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :owners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
   namespace :owner do
     root to: "home#index"
     resources :rooms
     resources :owner_profiles, except: [:index]
+  end
+
+  namespace :user do
+    root to: "home#index"
   end
 end
