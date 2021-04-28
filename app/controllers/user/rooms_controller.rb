dda5a29a7ec8a2aaaafd38cb2c8c99c1871b91b7
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-04-28 14:11:15 +0900

    add rooms index on user side

---
+++ app/controllers/user/rooms_controller.rb
@@ -0,0 +1,14 @@
+class User::RoomsController < User::ApplicationController
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
   namespace :user do
     root to: "home#index"
+    resources :rooms, only: [:index, :show]
   end
 end
